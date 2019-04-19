68401af3d313443b726665ef71fb9a0b6574d0bc
Shane Lee <shaneleephoto@gmail.com>
2019-04-19 20:04:38 +0100

    Add files via upload

---
+++ python/ArrangingCoins.py
@@ -0,0 +1,15 @@
+'''
+
+Coding Problem:   https://leetcode.com/problems/arranging-coins/
+
+Statistics: 
+
+  Runtime: 40 ms, faster than 100.00% of Python3 online submissions for Arranging Coins.
+  Memory Usage: 13.2 MB, less than 5.17% of Python3 online submissions for Arranging Coins.
+
+'''
+
+import math
+class Solution:
+    def arrangeCoins(self, n):
+        return int(-0.5 + math.sqrt(1 * 2 * n + 0.25))
